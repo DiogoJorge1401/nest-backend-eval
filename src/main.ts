a1aa2ba665acb148e85ba48b32a6c2317d16e292
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-12-12 12:09:40 -0300

    feat: integrate Swagger documentation and add metadata for API models

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import cookieParser from 'cookie-parser';
 import e from 'express';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import './utils/cloudinary';
+import metadata from './metadata';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
   });
+
+  await SwaggerModule.loadPluginMetadata(metadata);
+
+  const config = new DocumentBuilder()
+    .setTitle('User Transactions API')
+    .setVersion('1.0')
+    .addCookieAuth('access_token', { type: 'http' })
+    .build();
+
+  SwaggerModule.setup('/v1/api/docs', app, () =>
+    SwaggerModule.createDocument(app, config),
+  );
 
   app.setGlobalPrefix('/v1/api');
 
