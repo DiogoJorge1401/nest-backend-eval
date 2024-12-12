a98fc2cddb532e9c0d60786cec520f00a2ecb030
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-12-12 13:41:31 -0300

    feat: add API operation summary for update password endpoint

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Put, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
 import { UpdatePasswordDto } from './dtos/update-password.dto';
 import { UsersService } from './users.service';
-import { ApiCookieAuth } from '@nestjs/swagger';
+import { ApiCookieAuth, ApiOperation } from '@nestjs/swagger';
 
 @Controller('users')
 @UseGuards(JwtAuthGuard)
@@ -11,6 +11,9 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Put('/password')
+  @ApiOperation({
+    summary: 'Atualiza a senha do usuário autenticado.',
+  })
   updatePassword(@Request() req, @Body() body: UpdatePasswordDto) {
     return this.usersService.updatePassword(req.user._id, body);
   }
