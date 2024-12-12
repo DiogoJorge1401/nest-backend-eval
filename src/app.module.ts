ee3a278a623999cbd936001bd4e578f68454305f
DiogoJorge1401 <diogojorge1401@gmail.com>
2024-12-12 12:21:06 -0300

    feat: add modules for users, auth, kyc, and transactions with configuration for MongoDB and Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,31 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { KycModule } from './modules/kyc/kyc.module';
+import { TransactionsModule } from './modules/transactions/transactions.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.DB_URI,
+      }),
+    }),
+    RedisModule.forRootAsync({
+      useFactory: () => ({
+        type: 'single',
+        url: process.env.REDIS_URL,
+      }),
+    }),
+    UsersModule,
+    AuthModule,
+    KycModule,
+    TransactionsModule,
+  ],
   controllers: [],
   providers: [],
 })
